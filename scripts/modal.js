e0ec7649ae21d8d9f5df65765fec8e722933105d
kyoung3212 <kevin.young3212@gmail.com>
2018-10-04 16:52:08 -0700

    progress

--- scripts/modal.js
+++ scripts/modal.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
     SGT: {
       title: 'Student Grade Table',
       tag: 'FULL-STACK STUDENT GRADE TRACKING SYSTEM.',
-      detail: 'The Student Grade table is a full-stack web application that utilizes PHP calls to access the customized MySQL tables to keep track of the student grades for the given year, along with the class average. Utilizes CRON to regularly scrub data to protect from hackers and profanity. ',
+      detail: 'A Content management system (CMS) that allows for the ability to create, read, and delete student grade information from a database.  As a full-stack web application that utilizes PHP calls to access the customized MySQL tables to keep track of the student grades for the given year, along with the class average, SGT utilizes CRON to regularly scrub data to protect from hackers and profanity. ',
       link: 'https://www.kevin-young.us/SGT'
     },
     never: {
